6d3cf0a5c67c16b27c4ee574a16d0fbd13787bce
Germain LEFEBVRE <germain.lefebvre@ineat-conseil.fr>
2019-08-27 10:22:37 +0200

    chore(doc): Release documentation

--- deploy/release.sh
+++ deploy/release.sh
@@ -2,15 +2,18 @@
 
 if [ -z "$1" ] ; then
 	echo "Usage: deploy/release.sh <RELEASE_TAG_CURRENT>"
-	echo "         RELEASE_TAG_CURRENT: Release Tag lik 3.0.1.dev"
+	echo "         RELEASE_TAG_CURRENT: Release Tag like 3.0.1.dev"
 	exit 128
 fi
 
 RELEASE_TAG_CURRENT=$1
 RELEASE_TAG_PREVIOUS=$(git tag -l | tail -1)
 
-sed -i "s/^\(  version=\) .*/\1'${RELEASE_TAG_CURRENT}',/g" setup.py
-sed -i 's/^\(unreleased_version_label = \).*/\1"'${RELEASE_TAG_CURRENT}'"/g' .gitchangelog.rc
+sed -i "s/^\(  version=\).*/\1'${RELEASE_TAG_CURRENT}',/g" setup.py
 
-./gitchangelog.py ^${RELEASE_TAG_PREVIOUS} HEAD > changelogs/CHANGELOG-${RELEASE_TAG_CURRENT}.rst
+pipenv run gitchangelog ^${RELEASE_TAG_PREVIOUS} HEAD > changelogs/CHANGELOG-${RELEASE_TAG_CURRENT}.rst
+git add changelogs/CHANGELOG-${RELEASE_TAG_CURRENT}.rst setup.py
+git commit -m 'chore(changelog): Changelog for version '$RELEASE_TAG_CURRENT
+git push origin master
 git tag -a $RELEASE_TAG_CURRENT -F changelogs/CHANGELOG-${RELEASE_TAG_CURRENT}.rst
+git push origin ${RELEASE_TAG_CURRENT}
